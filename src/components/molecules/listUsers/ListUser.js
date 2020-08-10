9fdf7cfad90534da53d3de8f727b94899426497b
Aline Borges <ap.borges@live.com>
2020-08-09 23:48:46 -0300

    change the margins of name, user name and profile picture

--- src/components/molecules/listUsers/ListUser.js
+++ src/components/molecules/listUsers/ListUser.js
@@ -9,13 +9,13 @@ import Button from '../../atoms/buttons/Button';
 const ListUser = () => {
   return(
     <Container direction='row'>
-      <ImageProfile  src='https://avatars1.githubusercontent.com/u/3043525?v=4' alt='photoprofile' width='80px' margin='15px 15px 0 0'/>
-      <Container direction='column' margin='10px 0 0 0' justify='left' align='center'>
-        <Container direction='row' justify='right' align='flex-end' margin='12px 0 5px 0'>
-          <Name size='18px' weight='500' color='#F9F3F3' margin='10px 100px 0 0'>Guilherme Oliveira</Name>
-          <Username size='12px' weight='normal' color='#F9F3F3' margin='0 15px 5px 0'>@guibfo</Username>
+      <ImageProfile  src='https://avatars1.githubusercontent.com/u/3043525?v=4' alt='photoprofile' width='150px' margin='15px 15px 0 0'/>
+      <Container direction='column' margin='10px 0 0 0' justify='left' align='flex-start'>
+        <Container direction='row' justify='left' align='flex-end' margin='12px 0 5px 0'>
+          <Name size='18px' weight='500' color='#F9F3F3' margin='10px 15px 0 0'>Guilherme Oliveira</Name>
+          <Username size='12px' weight='normal' color='#F9F3F3' margin='50px 2px 5px 0'>@guibfo</Username>
         </Container>
-        <Container direction='row' margin='2px 85px 10px 0'>
+        <Container direction='row' margin='2px 120px 10px 0'>
           <Container direction='row'>
           <svg id='place' width="18" height="18" viewBox="0 0 23 23" fill="#8752CC" xmlns="http://www.w3.org/2000/svg">
             <path d="M11.0232 1.83716C7.46823 1.83716 4.59302 4.71237 4.59302 8.26734C4.59302 13.09 11.0232 20.2091 11.0232 20.2091C11.0232 20.2091 17.4534 13.09 17.4534 8.26734C17.4534 4.71237 14.5782 1.83716 11.0232 1.83716ZM11.0232 10.5638C10.4141 10.5638 9.83001 10.3219 9.39933 9.8912C8.96866 9.46053 8.7267 8.87641 8.7267 8.26734C8.7267 7.65827 8.96866 7.07415 9.39933 6.64347C9.83001 6.2128 10.4141 5.97085 11.0232 5.97085C11.6323 5.97085 12.2164 6.2128 12.6471 6.64347C13.0777 7.07415 13.3197 7.65827 13.3197 8.26734C13.3197 8.87641 13.0777 9.46053 12.6471 9.8912C12.2164 10.3219 11.6323 10.5638 11.0232 10.5638Z" fill="#8752CC" />
