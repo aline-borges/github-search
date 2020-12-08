6b1aeab579c504e00e208cb7ede48b05c0664c76
Aline Borges <ap.borges@live.com>
2020-12-07 22:02:54 -0300

    (docs): change size of container and logo from px to rem

--- src/components/organisms/header/Header.js
+++ src/components/organisms/header/Header.js
@@ -5,8 +5,8 @@ import AppName from '../../molecules/appName/AppName';
 
 const Header = () => {
   return(
-    <Container direction='row' justify='space-between' align='center' margin='15px 0 0'>
-      <Logo margin='0 0 0 140px' alt='logo' width='36px' />
+    <Container direction='row' justify='space-between' align='center' margin='1.5rem 0 0'>
+      <Logo margin='0 0 0 14rem' alt='logo' width='3.6rem' />
       <AppName />
     </Container>
   );
