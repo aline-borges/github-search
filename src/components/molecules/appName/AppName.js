19b98bd7fda5088fde431f2f61cf16a537c51f66
Aline Borges <ap.borges@live.com>
2020-12-09 19:46:34 -0300

    (docs): change the size type of the px container properties to rem

--- src/components/molecules/appName/AppName.js
+++ src/components/molecules/appName/AppName.js
@@ -5,7 +5,7 @@ import Subtitle from '../../../components/atoms/texts/Subtitle';
 
 const AppName = () => {
   return(
-    <Container direction='row' align='center' margin='0 160px 0 0'>
+    <Container direction='row' align='center' margin='0 16rem 0 0'>
       <Title>GitHub</Title>
       <Subtitle>Search</Subtitle>
     </Container>
