e19d78024374a6804741f4e9d5cabefda23c1214
Aline Borges <ap.borges@live.com>
2023-01-02 00:15:51 -0200

    fix(change root): import createRoot

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoute from './routes';
 import Global from './assets/styles/Global';
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <BrowserRouter>
     <Global />
     <AppRoute />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
